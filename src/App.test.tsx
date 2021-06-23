e19e9abdcd2e704378ff4d2713752b68ce77f70d
KIYO1234 <getstarted3601@gmail.com>
2021-06-23 15:33:36 +0900

    testの環境設定

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('2 plus 2', () => {
+  const value = 2 + 2
+  expect(value).toBe(4)
+})
+
+
+
+export {}
